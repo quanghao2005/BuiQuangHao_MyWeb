4e87230c9f0a4ab627445e2d4fecc47dbd698c50
Bùi Quang Hào <hao01679462492@gmail.com>
2026-07-14 14:08:11 +0700

    add lab13

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+// File mặc định của Laravel
 import './bootstrap';
+
+// Import thư viện Bootstrap từ node_modules
+import 'bootstrap';
+
+// Import file JavaScript tự viết
+import './preview-image';
--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'My Web')</title>
 
-    {{-- CDN Bootstrap CSS --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- Sử dụng CSS và JavaScript thông qua Vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- CDN Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -42,9 +42,6 @@
         </div>
     </div>
 
-    {{-- CDN Bootstrap JavaScript --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('js/preview-image.js') }}"></script>
     @stack('scripts')
 </body>
 
